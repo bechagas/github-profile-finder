0de1a0e0b66d204ce71c968ebf6a35c868846264
bechagas <bechagas.chagas@gmail.com>
2026-06-04 18:55:06 -0300

    fix: fix the repository URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/github-profile-search/",
+  base: "https://github.com/bechagas/github-profile-search.git",
   plugins: [react()]
 });
